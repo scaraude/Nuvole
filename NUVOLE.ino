7727175f8cb120d2205271ed2c86e976888a4731
scaraude <ludodante@hotmail.fr>
2020-03-19 18:00:07 +0100

    FUCKING VERSION presque STABLE - la main gauche ne s'efface pas sur nuvole

--- NUVOLE.ino
+++ NUVOLE.ino
@@ -4,6 +4,23 @@
 #include <Adafruit_NeoPixel.h>
 #include <LiquidCrystal.h>
 
+//---------------------------CONF TAB--------------------------
+#define NOTEREC 60
+
+//--------------------------------LED COLORS------------------
+#define BLEU pixels.Color(0,0,255)
+#define ROUGE pixels.Color(255,0,0)
+#define VERT pixels.Color(0,255,0)
+#define TURQUOISE pixels.Color(0,150,150)
+#define ORANGE pixels.Color(150,50,0)
+
+//--------------------------------CONFIG LED------------------
+#define PIN        22 // On Trinket or Gemma, suggest changing this to 1
+#define BRIGHTNESS 100
+#define NUMPIXELS 144 // Popular NeoPixel ring size
+
+//--------------------------------DECL. LED------------------
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 //--------------------------------DECL. FILE------------------
 File myFile;
 
@@ -26,12 +43,16 @@ note *stockP, *stockR;
 uint8_t *compteurP, *compteurR;
 
 bool JouerDansDroit;
-uint8_t nextMesure = 0;
+uint8_t nextMesure;
+int tempo = 40;
+uint8_t curseurP;
+static unsigned long tRec;
 
 
 
 void setup() {
   // put your setup code here, to run once:
+
 
   //--------------------INIT SERIAL-----------------
   Serial.begin(9600);
@@ -47,44 +68,135 @@ void setup() {
   }
   Serial.println("initialization done.");
 
+  //--------------------INIT LED-----------------
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setBrightness(BRIGHTNESS);
+  pixels.show();   // Send the updated pixel colors to the hardware.
+
   //--------------------INIT VARIABLE-----------------
   InitVariables();
+
+  //------------------------FILE-----------------
+  myFile = SD.open("Nuvole.txt");
+
+  // -------------------PREMIER REMPLISSAGE---------
+  bool recFini = false;
+
+  Rec();
+
+  while (!recFini)
+  {
+    //Serial.println("Dans 3 eme boucle");
+    recFini = Rec();
+  }
+  Aiguillage();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  myFile = SD.open("Nuvole.txt");
   bool recFini = false, musiqueFini = false;
-  
+
   while (!musiqueFini)
   {
-    while (!recFini)
+    //Serial.println(*compteurP);
+    //Serial.println("Dans 1 eme boucle");
+    while (*compteurP != 0)
     {
-      recFini = Rec();
+      //Serial.println("Dans 2 eme boucle");
+      while (!recFini)
+      {
+        //Serial.println("Dans 3 eme boucle");
+        Lecture();
+        recFini = Rec();
+      }
+      Lecture();
     }
     musiqueFini = Aiguillage();                           //inverse le stock joué et le stock enregistré
-    Affiche();
+    //Affiche();
+
     recFini = false;
+    tRec = micros();
   }
 
-  /*while (*compteurP != 0)
-    {
+  pixels.show();
+  tRec = micros();
+}
+
+void Lecture()
+{
+  float utemps = 60000000 / (tempo *4);
+  static bool offPassed = false;
+
+  if (micros() >= tRec + utemps * 0.5 && !offPassed) // check si il faut eteindre
+  {
+    curseurP++;
     LedOff();
+    //Serial.println("into LEDOFF");
+    offPassed = true;
+  }
+
+  if (micros() >= tRec + utemps)
+  {
     LedOn();
-
-    LedOff();
-    LedOn();
-    }*/
-
-
-  myFile.close();
-
-  Serial.println("an other time ?");
-
-  while (!Serial.available()) {
-    ; // Wait for enter key;
+    tRec = micros();
+    offPassed = false;
+    //Serial.println("into LEDON");
   }
 }
+
+void LedOff()
+{
+  for (int i = 0; i <= *compteurP; i++)
+  {
+    if (curseurP >= (stockP[i].tdebut + stockP[i].duree))
+    {
+      pixels.setPixelColor(stockP[i].led, 0);
+      pixels.show();
+
+      stockP[*compteurP].led = 0;
+      stockP[*compteurP].tdebut = 0;
+      stockP[*compteurP].duree = 0;
+      stockP[*compteurP].main = 0;
+
+      for (int t = 1; stockP[i + t].led != 0; t++)
+      {
+        stockP[i + t - 1].led = stockP[i + t].led;
+        stockP[i + t].led = 0;
+        stockP[i + t - 1].tdebut = stockP[i + t].tdebut;
+        stockP[i + t].tdebut = 0;
+        stockP[i + t - 1].duree = stockP[i + t].duree;
+        stockP[i + t].duree = 0;
+        stockP[i + t - 1].main = stockP[i + t].main;
+        stockP[i + t].main = 0;
+      }
+
+      if (*compteurP > 0)
+      {
+        (*compteurP)--;
+        i--;
+      }
+    }
+  }
+}
+
+void LedOn()
+{
+  for (int i = 0; i <= *compteurP; i++)
+  {
+    if (stockP[i].tdebut == curseurP)
+    {
+      switch (stockP[i].main)
+      {
+        case 1 : pixels.setPixelColor(stockP[i].led, BLEU); break;
+        case 2 : pixels.setPixelColor(stockP[i].led, ROUGE); break;
+        case 3 : pixels.setPixelColor(stockP[i].led, TURQUOISE); break;
+        case 4 : pixels.setPixelColor(stockP[i].led, ORANGE); break;
+      }
+      pixels.show();
+    }
+  }
+}
+
 
 void InitVariables()
 {
@@ -105,6 +217,11 @@ void InitVariables()
   //---------------------------INIT POINTEUR
   compteurP = &compteurDroit;
   compteurR = &compteurGauche;
+
+  nextMesure = 0;
+  curseurP = 0;
+
+  tRec = 0;
 }
 
 bool Rec()
@@ -174,6 +291,9 @@ bool Aiguillage()                       //INVERSE LES TABLEAUX JOUER ET ENREGIST
 
     JouerDansDroit = true;
   }
+
+  curseurP = 0;
+  Affiche();
 
   if (!myFile.available())
     return true;
